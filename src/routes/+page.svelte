e3db835d07a56adebcbb65c94f0c97f87dfbaa90
Steve Godin <steve.god@gmail.com>
2022-12-24 16:24:26 -0500

    Add advent page and rick roll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
-	console.log('Test');
+	console.log('%cWelcome to the neckbear club! 🎅', 'color:green;font-weight:bold;');
+	console.log(
+		'%cI forgot to show a link. Can you use your inspection skills to find it?',
+		'color:red;'
+	);
 </script>
 
-<p>Where do pros hangout in browsers?</p>
+<p>Where do developers hangout in browsers?</p>
+
+<a hidden href="/advent/1">On the first day of Christmas, My good friends brought to me...</a>
